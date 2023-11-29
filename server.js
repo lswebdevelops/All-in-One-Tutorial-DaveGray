aa3b248d254dad0445b81f05c6cf6962a3493293
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-28 22:29:16 -0300

    basic files for q webserver

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('HI');
+const logEvents = require("./logEvents");
+const EventEmitter = require("events");
+class Emitter extends EventEmitter {}
+
+const myEmitter = new Emitter();
+
+
+
+
+
+
+
+
+
+// myEmitter.on("log", (msg) => logEvents(msg));
+
+// setTimeout(() => {
+//   // emit event
+
+//   myEmitter.emit("log", "Log event emitted!");
+// }, 20);
